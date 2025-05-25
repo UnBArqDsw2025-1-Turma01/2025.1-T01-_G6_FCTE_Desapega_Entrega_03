f6bdacbfca07ea35ef4850c1e085727fd448bf6b
ccarlaa <carlacarlaclementino@gmail.com>
2025-05-25 12:32:38 -0300

    add storybook config

---
+++ fcte-desapega/src/components/Layout/Layout.stories.jsx
@@ -0,0 +1,14 @@
+import Layout from './index';
+
+export default {
+  title: 'Components/Layout',
+  component: Layout,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {},
+};
