1ba556bdc18a2d15a5c072709ce207e440f5c736
ccarlaa <carlacarlaclementino@gmail.com>
2025-05-28 22:36:49 -0300

    add badge component

---
+++ frontend/src/components/Badge/layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BadgeLayout = ({
+    children
+}) => {
+    return (
+        <div className="rounded-full w-fit cursor-pointer font-medium border-[1.5px] border-secondary-500 overflow-hidden">
+            {children}
+        </div>
+    )
+}
+
+export default BadgeLayout;
