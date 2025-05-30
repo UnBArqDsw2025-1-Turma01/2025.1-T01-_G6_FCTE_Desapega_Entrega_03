08e42981aeae9a29bc88eba34ea286622717867d
Daniela Alarcão <danielaalarcao21@gmail.com>
2025-05-30 17:38:37 -0300

    componente Dropdown utilizando o padão de projeto Builder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import Layout from "./components/layout/index/"
+import Layout from "./components/layout/index/";
 import UserPopover from "./components/Layout/UserPopover";
+import { DropdownBuilder } from "./components/Layout/DropdownBuilder";
 
 export default function App() {
   return (
-        <Layout>
-
+    <Layout>
       {/* Frase grandona antes do carrossel */}
       <section className="text-center py-16 bg-white">
         <h1 className="text-5xl font-bold text-gray-800">
           DESAPEGOS DA <span className="text-orange-500">UNB</span> <br /> EM UM SÓ LUGAR
         </h1>
         <h2 className="mt-4 text-xl font-light text-gray-500 tracking-wide">
-          <strong> A comunidade da UnB em movimento</strong>
+          <strong>A comunidade da UnB em movimento</strong>
         </h2>
       </section>
     </Layout>
