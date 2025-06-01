0c9b50bdcf0f6861eaea571a602f122acba5ad8e
ccarlaa <carlacarlaclementino@gmail.com>
2025-06-01 20:34:08 -0300

    fixes footer]

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import Logo from "../../assets/Logo.png"; 
-
+import { ArrowUpIcon } from "@heroicons/react/24/solid";
 const Footer = () => {
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
   return (
-    <footer className="bg-primary-800 text-white px-8 py-6">
-      <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between max-w-7xl mx-auto">
-        {/* Logo e texto abaixo */}
-        <div className="flex flex-col">
-          <img src={Logo} alt="Logo" className="w-40 mb-3" />
-          <p className="text-sm text-white/80 max-w-xs">
-            O Repassei é o cantinho da UnB onde estudantes trocam, vendem e acham tesouros escondidos. De livro a bikes, aqui tudo encontra um novo lar com carinho universitário. 
-          </p>
+    <footer className="bg-primary-800 text-white py-6 flex justify-center">
+      <div className="min-w-[1024px]">
+        <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between mx-auto">
+          {/* Logo e texto abaixo */}
+          <div className="flex flex-col">
+            <img src={Logo} alt="Logo" className="w-40 mb-3" />
+            <p className="text-sm text-white/80 max-w-xs">
+              O Repassei é o cantinho da UnB onde estudantes trocam, vendem e acham tesouros escondidos. De livro a bikes, aqui tudo encontra um novo lar com carinho universitário. 
+            </p>
+          </div>
         </div>
-      </div>
 
-      <hr className="my-6 border-white/40" />
+        <div className="h-px w-full bg-white my-4" />
 
-      <div className="text-left text-xs text-white/70 max-w-7xl mx-auto">
-        © 2025 Repassei. Todos os direitos reservados. Desenvolvido com carinho para a comunidade da UnB.
+        <div className="flex items-center justify-between">
+          <div className="text-left text-xs text-white/70">
+            © 2025 Repassei. Todos os direitos reservados. Desenvolvido com carinho para a comunidade da UnB.
+          </div>
+          <div 
+            onClick={scrollToTop}
+            className="h-12 w-12 cursor-pointer bg-secondary-600 rounded-full flex items-center justify-center hover:bg-secondary-700 transition-colors">
+            <ArrowUpIcon className="w-6 h-6 text-white" />
+          </div>
+        </div>
       </div>
     </footer>
   );
