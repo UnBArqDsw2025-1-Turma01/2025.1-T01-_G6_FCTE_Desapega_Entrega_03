4d11aba6433a8182793b48843a530580a62a51fe
Renata Kurzawa <rekurzawa28@gmail.com>
2025-06-02 12:15:55 -0400

    resolvendo conflito

--- backend/src/strategy/emailUnbLogin.strategy.js
+++ backend/src/strategy/emailUnbLogin.strategy.js
@@ -15,23 +15,3 @@ async function authenticate({ email, password }) {
 }
 
 module.exports = { authenticate };
-
-const { User } = require('../models');
-const bcrypt = require('bcrypt');
-
-class EmailUnbLoginStrategy {
-    async authenticate({ email, password }) {
-        if (!email.endsWith('unb.br')) {
-            throw new Error('Apenas emails da UnB são permitidos.');
-        }
-
-        const user = await User.findOne({ where: { email } });
-        if (!user || !(await bcrypt.compare(password, user.password))) {
-            throw new Error('Email ou senha inválidos.');
-        }
-
-        return user;
-    }
-}
-
-module.exports = EmailUnbLoginStrategy;
