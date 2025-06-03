d7d85681db1841e7ff6cadc52c10b3dd7ee3bd8e
Daniela Alarcão <danielaalarcao21@gmail.com>
2025-06-02 21:03:27 -0300

    Daniela Facade Composite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Anuncios from "./pages/Anuncios";
+import Cadastro from "./pages/Cadastro";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout logged={false} />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/anuncios" element={<Anuncios />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout logged={false} />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/anuncios" element={<Anuncios />} />
+        </Route>
+        <Route element={<Layout logged={true} />}>
+           <Route path="/cadastro" element={<Cadastro />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
